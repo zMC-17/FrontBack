6635779fbad82f33831c940cf147403d58f68187
german_sd <stropus.gd@gmail.com>
2026-02-27 18:27:38 +0300

    Third practice

--- Practice_2/app.js
+++ Practice_2/app.js
@@ -1,52 +1,59 @@
-    // Необходимо реализовать API, которое предоставляет CRUD операции для списка
-    // товаров (просмотр всех товаров, просмотр товара по id, добавление товара,
-    // редактирование товара, удаление товара).
-    // Объект товара должен содержать следующие поля: id, название, стоимость.
-
-    const express = require('express')
-    const app = express()
-    const port = 3000
+const express = require('express')
+const app = express()
+const port = 3000
 
 
-    let products = [
-        { id: 1, name: "Ноутбук", price: 51999 },
-        { id: 2, name: "Планшет", price: 22999 },
-        { id: 3, name: "Фен", price: 6999 }
-    ]
+let products = [
+    { id: 1, name: "Ноутбук", price: 51999 },
+    { id: 2, name: "Планшет", price: 22999 },
+    { id: 3, name: "Фен", price: 6999 }
+]
 
-    app.use(express.json())
+app.use(express.json())
 
-    app.get('/', (req, res) =>{
-        res.send("Сервер работает исправно")
-    })
+app.get('/', (req, res) =>{
+    res.send("Сервер работает исправно")
+})
 
-    app.get('/products', (req, res) => {
-        res.json(products)
-    })
+app.get('/products', (req, res) => {
+    res.json(products)
+})
 
-    app.get('/products/:id', (req, res) => {
-        let product_info = products.find((p) => p.id == req.params.id)
-        res.json(product_info)
+app.get('/products/:id', (req, res) => {
+    let product_info = products.find((p) => p.id == req.params.id)
+    if (!product_info) {
+        res.status(404).json({message: 'Продукт не найден'})
+    }
+    res.json(product_info)
 
-    })
+})
 
-    app.post('/products/', (req, res) => {
-        let product_name = req.body.name
-        let product_price = req.body.price
-        let product_id = req.body.id
-        products.push({id: product_id, name: product_name, price: product_price})
-        res.status(201).json({
-        message: 'Пользователь создан',
-        user: req.body // Эхо: верни те же данные обратно
-    });
-    })
+app.post('/products/', (req, res) => {
+    let product_name = req.body.name
+    let product_price = req.body.price
+    let product_id = req.body.id
+    products.push({id: product_id, name: product_name, price: product_price})
 
-    app.delete('/products/:id', (req, res) => {
-        products.splice(parseInt(req.params.id), 1)
+    res.status(201).json({
+    message: 'Пользователь создан',
+    user: req.body // Эхо: верни те же данные обратно
+});
+})
+
+app.delete('/products/:id', (req, res) => {
+    let request_id = req.params.id
+    let index = products.findIndex(product => product.id == request_id)
+
+    if (index === -1) {
+        res.status(404).json({message: 'Элемент отсутствует'})
+    } else {
+        products.splice(index, 1)
         res.status(200).json({message: 'Успешно удалено'})
+    }
 
-    })
 
-    app.listen(port, () => {
-        console.log(`Сервер запущен на http://localhost:${port}`)
-    })
+})
+
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`)
+})
---
+++ Practice_3/report.md
@@ -0,0 +1,39 @@
+# Запросы к собственному API с товарами (2 практика)
+
+1. Выводим список всех товаров:
+![First](./screenshots/first.jpg)
+
+2. Добавляем новый элемент post запросом:
+!['Second'](./screenshots/second.jpg)
+
+Проверяем, действительно ли добавлился такой элемент get запросом по id:
+!['Second_result'](./screenshots/second_result.jpg)
+
+3. Пробуем удалить этот элемент delete запросом:
+!['Third'](./screenshots/third.jpg)
+
+Проверяем, что элемент действительно теперь отсутствует
+![Third_result](./screenshots/third_result.jpg)
+
+
+# Запросы к публичному API
+
+
+## API для получения новостей GNews.io
+
+1. Запрос на поиск новости с ключевыми словами "Россия" и "Москва":
+![first_news](./screenshots/news1.png)
+
+2. Запрос на новости по тематике "Бизнес":
+![alt text](./screenshots/news2.png)
+
+## API для получение UUID-ключа (без apikey)
+
+3. GET запрос на получения 2-х UUID ключей
+![alt text](./screenshots/UUID.png)
+
+4. API - рецепт случайного блюда
+![alt text](./screenshots/dish.png)
+
+5. API - список блюда с конкретным ингридиентом из query запроса (куриные грудки)
+![alt text](./screenshots/chicken_breasts.png)
